f89a73e1ebd63bc494c6a84b21493c9ba31093ba
Harshith MohanKumar <hiharshith18@gmail.com>
2019-11-09 03:10:14 +0530

    Updated

--- Person.py
+++ Person.py
@@ -1,9 +1,9 @@
 #########################################################################
-#								Person.py								#
-#	Version: 1.0														#
-#	Date: 11/02/2019													#
-#	Description:	This module holds the Person class which hold all 	#
-#					the desired information about the individual		#
+#	Person.py								
+#	Version: 1.0														
+#	Date: 11/02/2019													
+#	Description:	This module holds the Person class which hold all 
+#					the desired information about the individual		
 #########################################################################
 
 class Person:
@@ -15,6 +15,10 @@ class Person:
 	'''
 
 	def __init__(self, name, phone, email, address, birthday, favorites):
+		'''
+		This is the constructor. We define and declare all of the values
+		which are supposed to contained within a contact
+		'''
 		self.name = name
 		self.phone = phone
 		self.email = email
@@ -36,3 +40,18 @@ class Person:
 		Returns: boolean True/False
 		'''
 		return self.fav
+
+	def display_info(self):
+		'''
+		This method will display all of the information which is contained
+		about the person
+		Parameters: none
+		Returns: none
+		'''
+		print('\n\n\n')
+		print("Name:			",self.name)
+		print('Phone Number:		',self.phone)
+		print('Email:			',self.email)
+		print('Residential address:	',self.address)
+		print('Birthday:		',self.birthday[0:2]+'/'+self.birthday[2:4]+'/'+self.birthday[4:])
+		print('\n\n\n')
