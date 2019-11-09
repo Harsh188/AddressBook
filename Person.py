0d5dd31214c4bef2d3094e78e21b60131a428d81
Harshith MohanKumar <hiharshith18@gmail.com>
2019-11-09 17:55:30 +0530

    Added more accessor methods

--- Person.py
+++ Person.py
@@ -32,7 +32,19 @@ class Person:
 		Returns: 	name of individual
 		'''
 		return self.name
+
+	def get_phone(self):
+		return self.phone
+
+	def get_email(self):
+		return self.email
+
+	def get_add(self):
+		return self.address
 	
+	def get_birthday(self):
+		return self.birthday
+
 	def get_fav(self):
 		'''
 		The get_fav function is used to access the if the individual
@@ -40,6 +52,7 @@ class Person:
 		Returns: boolean True/False
 		'''
 		return self.fav
+
 
 	def display_info(self):
 		'''
