4dff430dc8073e124fdb6e910be31ce44555c9c5
Zayd-Jamadar <ZaydJamadar777@gmail.com>
2019-11-11 23:08:48 +0530

    Second commit

--- TESTING/gui.Test02.py
+++ TESTING/gui.Test02.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 
+
+def click():
+    Search_win = tk.Toplevel(root, height=HEIGHT, width=WIDTH)
+    Search_win.title("Search Contact")
+    Search_win.configure(background='#420666')
+    bw1 = tk.Button(Search_win, text="Close", command=Search_win.destroy)
+    bw1.place(relwidth=0.1,relheight=.1,relx=.45,rely=.8)
+
+
 HEIGHT = 400
 WIDTH = 400
+
 
 root = tk.Tk()
 root.title("ADDRESS-BOOK")
@@ -10,17 +20,25 @@ root.title("ADDRESS-BOOK")
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg="#80c1ff", bd=5)
-frame.place(relwidth=0.75, relheight=0.1, relx=0.5, rely=0.1, anchor="n")
+frame = tk.Frame(root, bg="#420666", bd=5)
+frame.place(relwidth=1, relheight=1)
 
-entry = tk.Entry(frame, font=40)
-entry.place(relwidth=0.65, relheight=1)
+# entry = tk.Entry(frame, font=40)
+# entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Contact")
-button.place(relx=0.7, relwidth=0.3, relheight=1)
+b1 = tk.Button(frame, text="Search", command=click)
+b1.place(relx=0.25, relwidth=0.5, relheight=0.25)
 
-lower_frame = tk.Frame(root, bg="#80c1ff", bd=5)
-lower_frame.place(relwidth=0.75, relheight=0.6, relx=0.5, rely=0.25, anchor="n")
 
-label = tk.Label(lower_frame, text="Details will be displagi
+b2 = tk.Button(frame, text="Add")
+b2.place(relx=0.25, rely=0.3, relwidth=0.5, relheight=0.25)
+
+b3 = tk.Button(frame, text="Edit")
+b3.place(relx=0.25, rely=0.65, relwidth=0.5, relheight=0.25)
+
+# lower_frame = tk.Frame(root, bg="#80c1ff", bd=5)
+# lower_frame.place(relwidth=0.75, relheight=0.6, relx=0.5, rely=0.25, anchor="n")
+
+# label = tk.Label(lower_frame, text="Details will be displayed here")
+
 root.mainloop()
