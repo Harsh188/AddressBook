0da4b8a48bc8d8f4e5ada6428ea4244cb4e8d8d5
Zayd-Jamadar <ZaydJamadar777@gmail.com>
2019-11-15 00:35:19 +0530

    15/11/19

--- new_interface.py
+++ new_interface.py
@@ -11,12 +11,13 @@ canvas.pack()
 f1 = tk.Frame(root, bg="#373737", bd=5)
 f1.place(relwidth=0.25, relheight=1)
 
-textentry = tk.Entry(f1, width=20, bg="#ffffff")
-textentry.insert(0, "Enter Name")
-textentry.place(relx=0.039, rely=0.04, relwidth=0.925, relheight=0.035)
+textentry1 = tk.Entry(f1, width=20, bg="#ffffff")
+textentry1.insert(0, "Enter Name")
+textentry1.place(relx=0.039, rely=0.04, relwidth=0.925, relheight=0.035)
 
-lbx = tk.Listbox(f1)
-lbx.place(relx=0.039, rely=0.125, relwidth=0.925, relheight=0.875)
+lbx = tk.Listbox(f1,background="#373737",fg="white")
+lbx.place(relx=0.039, rely=0.125, relwidth=0.925, relheight=0.9)
+lbx.config(borderwidth=0)
 
 sbr = tk.Scrollbar(lbx)
 sbr.pack(side="right", fill="y")
@@ -26,5 +27,8 @@ lbx.config(yscrollcommand=sbr.set)
 
 for data in range(100):
     lbx.insert(data,"Sample data"+str(data+1))
+
+f2 = tk.Frame(root, bg="#2a2a2a", bd=5)
+f2.place(relx = 0.25,relwidth=0.75, relheight=1)
 
 root.mainloop()
