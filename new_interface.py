16f5b3b57375a9b4e2964b82cc7500b81a36fef1
Zayd-Jamadar <ZaydJamadar777@gmail.com>
2019-11-16 09:35:05 +0530

    Finished add , edit features

--- new_interface.py
+++ new_interface.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from tkinter import font as tkfont
 import Contact_book as cb
 
+
 def lift_frame(frame_name):
     frame_name.lift()
-
 
 
 root = tk.Tk()
@@ -40,10 +40,10 @@ f2.place(relx=0.25, relwidth=0.75, relheight=1)
 output1 = tk.Text(f2, background="#2a2a2a", fg="white")
 output1.place(relx=0.1, rely=0, relwidth=0.8, relheight=0.85)
 
-b1 = tk.Button(f2, text="+", font=("Helvetica", "24"),command=lambda:lift_frame(f4))
+b1 = tk.Button(f2, text="+", font=("Helvetica", "24"), command=lambda: lift_frame(f4))
 b1.place(relx=0.06, rely=0.928, relwidth=0.0655, relheight=0.045)
 
-b2 = tk.Button(f2, text="Edit", font=("Helvetica", "16"),command=lambda:lift_frame(f3))
+b2 = tk.Button(f2, text="Edit", font=("Helvetica", "8"), command=lambda: lift_frame(f3))
 b2.place(relx=0.75, rely=0.928, relwidth=0.0655, relheight=0.045)
 
 b3 = tk.Button(f2, text="Favourites", font=("Helvetica", "8"))
@@ -56,43 +56,46 @@ b3.config(bg="#a8a8a8", fg="white")
 f3 = tk.Frame(canvas, bg="#3f3f3f", bd=5)
 f3.place(relx=0.25, relwidth=0.75, relheight=1)
 
-b4 = tk.Button(f3, text="Back", font=("Helvetica", "8"),command=lambda:lift_frame(f2))
+b4 = tk.Button(f3, text="Back", font=("Helvetica", "8"), command=lambda: lift_frame(f2))
 b4.place(relx=0.85, rely=0.928, relwidth=0.0655, relheight=0.045)
 
-f4 = tk.Frame(canvas, bg="yellow", bd=5)
+f4 = tk.Frame(canvas, bg="#373737", bd=5)
 f4.place(relx=0.25, relwidth=0.75, relheight=1)
 
-b4 = tk.Button(f4, text="Back", font=("Helvetica", "8"),command=lambda:lift_frame(f2))
+b4 = tk.Button(f4, text="Back", font=("Helvetica", "8"), command=lambda: lift_frame(f2))
 b4.place(relx=0.85, rely=0.928, relwidth=0.0655, relheight=0.045)
 
 
 name = tk.Entry(f4, width=20, bg="yellow")
-name.insert(0, 'Enter Name')
+name.insert(0, "Enter Name")
 name.place(relx=0.30, rely=0.25, relwidth=0.4, relheight=0.05)
 phone = tk.Entry(f4, width=20, bg="yellow")
-phone.insert(0, 'Enter Phone Number')
+phone.insert(0, "Enter Phone Number")
 phone.place(relx=0.30, rely=0.30, relwidth=0.4, relheight=0.05)
 email = tk.Entry(f4, width=20, bg="yellow")
-email.insert(0, 'Enter Email')
+email.insert(0, "Enter Email")
 email.place(relx=0.30, rely=0.35, relwidth=0.4, relheight=0.05)
 addy = tk.Entry(f4, width=20, bg="yellow")
-addy.insert(0, 'Enter Address')
+addy.insert(0, "Enter Address")
 addy.place(relx=0.30, rely=0.40, relwidth=0.4, relheight=0.05)
 bd = tk.Entry(f4, width=20, bg="yellow")
-bd.insert(0, 'Enter Birthday')
+bd.insert(0, "Enter Birthday")
 bd.place(relx=0.30, rely=0.45, relwidth=0.4, relheight=0.05)
 fav = tk.Entry(f4, width=20, bg="yellow")
-fav.insert(0, 'Enter if Favourite(yes/no)')
+fav.insert(0, "Enter if Favourite(yes/no)")
 fav.place(relx=0.30, rely=0.50, relwidth=0.4, relheight=0.05)
 
-b5 = tk.Button(f4, text="Add Contact",command = lambda: cb.call_add([name.get(), \
-            phone.get(),email.get(),addy.get(),bd.get(),fav.get()]))
+b5 = tk.Button(
+    f4,
+    text="Add Contact",
+    command=lambda: cb.call_add(
+        [name.get(), phone.get(), email.get(), addy.get(), bd.get(), fav.get()]
+    ),
+)
 b5.place(relx=0.42, rely=0.60, relwidth=0.15, relheight=0.05)
-    
 
 
 f2.lift()
-
 
 
 root.mainloop()
